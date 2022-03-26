161eccc15d7fc9f06cb4bcd562735c271f379eef
joelle99762 <87580369+joelle99762@users.noreply.github.com>
2022-03-26 19:46:46 +0200

    2eme

--- client.js
+++ client.js
@@ -4,13 +4,13 @@ fetch('https://apivlavahassane.herokuapp.com/data')
       var RH=document.getElementById('RH');
       var originalcontent=`<div>
       <div class="icon"><i class="joelle"></i></div>
-      <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+      <h4 class="title"><a href="">hassane</a></h4>
       <p class="description">hassane</p>
     </div>`;
    for(var i=0;i<json.length;i++){
       var Mycontent=document.createElement('div');
       var costumise=originalcontent;
-      costumise=costumise.replace("Lorem Ipsum",json[i].title)
+      costumise=costumise.replace("hassane",json[i].title)
       costumise=costumise.replace("hassane",json[i].description)
       costumise=costumise.replace("joelle",json[i].icon)
       Mycontent.className='col-lg-4 col-md-6 icon-box';
